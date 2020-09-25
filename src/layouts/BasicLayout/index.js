f8b30346be81aa192f2a0968101a8a5993b2b44f
Aldair Valencia <aldairvm95@gmail.com>
2020-09-25 10:49:25 -0500

    added custom tag of open graph

--- src/layouts/BasicLayout/index.js
+++ src/layouts/BasicLayout/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import FooterSite from "../../componets/sections/Footer"
-import { LayoutWrapper, MainLayout, Container } from "./styled"
+import FooterSite from "../../componets/sections/Footer";
+import { LayoutWrapper, MainLayout, Container } from "./styled";
 
 export default function Layout({ children }) {
   return (
@@ -10,30 +10,36 @@ export default function Layout({ children }) {
           href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          href=""
-        />
-        <meta name="author" content="Aldair Valencia <aldairvm95@gmail>" />
+        <link rel="icon" type="image/png" href="" />
+        <meta name="author" content="Aldair Valencia" />
         <meta
           name="keywords"
-          content="Registro Imperium Cross"
+          content="Aplicación web para registrar de Alumnos de Imperium Cross"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>ImperiumCross | Registro</title>
+        <meta
+          property="og:title"
+          content="ImperiumCross | Registro de Alumnos"
+          key="ogtitle"
         />
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          property="og:description"
+          content="Aplicación web para registrar de Alumnos de Imperium Cross"
+          key="ogdesc"
+        />
+        <meta property="og:image" content="/logo.png" key="ogimage" />
+        <meta
+          property="og:site_name"
+          content="ImperiumCross"
+          key="ogsitename"
         />
       </Head>
 
       <MainLayout>
-        <Container>
-          {children}
-        </Container>
+        <Container>{children}</Container>
         <FooterSite />
       </MainLayout>
-      
-
     </LayoutWrapper>
-  )
+  );
 }
