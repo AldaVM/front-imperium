19791a502bb365303dc7ffa93b041d9abfa325b0
AldaVM <46821494+AldaVM@users.noreply.github.com>
2023-01-07 10:07:30 -0500

    update url backend

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 //https://imperium-backend.herokuapp.com/v1/api
 
-export const URL_API = "https://imperium-backend.herokuapp.com/v1/api";
+export const URL_API = "https://app-imperiumcross.herokuapp.com/v1/api";
